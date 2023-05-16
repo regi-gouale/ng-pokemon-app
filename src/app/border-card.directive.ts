8d2e292e04753669dfdbbe7d12b72c7bf59a5ca7
Régi GOUALE <74569854+regi-gouale@users.noreply.github.com>
2023-05-16 13:29:45 +0200

    Utiliser les directives

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[ppkmnBorderCard]'
+  selector: '[pkmnBorderCard]'
 })
 export class BorderCardDirective {
 
