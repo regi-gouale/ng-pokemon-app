354aad9c187855baa1d39f7819fe3b067b964073
Régi GOUALE <74569854+regi-gouale@users.noreply.github.com>
2023-05-16 11:42:44 +0200

    Composants Améliorer le composant racine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemon-list';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{ pokemonList[0] }}!</h1>`
+  template: `<h1>Liste de Pokémons</h1>`
 })
 export class AppComponent implements OnInit{
-  pokemonList:Array<string> = ['Pikachu', 'Salamèche', 'Bulbizarre', 'Carapuce'];
+  pokemonList:Array<Pokemon>= POKEMONS;
 
   ngOnInit(): void {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[2]);
+    this.selectPokemon(this.pokemonList[0]);
   }
 
-  selectPokemon(pokemonName: string): void {
-    console.log(`Vous avez cliqué sur le pokemon ${pokemonName}`);
+  selectPokemon(pokemon: Pokemon): void {
+    console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`);
   }
 
 }
