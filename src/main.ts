ed6d3a62ac9e13aa4ca95eebec6f8c989fba4a57
Régi GOUALE <74569854+regi-gouale@users.noreply.github.com>
2023-11-01 16:56:56 +0100

    Clean app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { provideRouter, Routes } from '@angular/router';
